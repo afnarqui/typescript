c03b5f31b8f8ccded662dc4c0bf54ea4471ae0ca
Andrés Naranjo <afnarqui9@gmail.com>
2021-05-23 11:59:21 -0500

    first commit

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,16 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+hp = 'FULL';
+
+export {
+    nombre, hp,estaVivo
+}
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+import {nombre, hp, estaVivo}  from './ejercicios/01-tipos-basicos';
+console.log(nombre, hp, estaVivo)
